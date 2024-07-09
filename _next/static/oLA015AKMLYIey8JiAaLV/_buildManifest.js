47ddf7b70f571e72f1aaa8ccd4cadeadf95dfcbe
utkarshexample <utkarshexample@users.noreply.github.com>
2024-07-09 04:28:03 +0000

    deploy: 7c139878e011366d05045503432915e72636df85

---
+++ _next/static/oLA015AKMLYIey8JiAaLV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-109ed95c4939c292.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
